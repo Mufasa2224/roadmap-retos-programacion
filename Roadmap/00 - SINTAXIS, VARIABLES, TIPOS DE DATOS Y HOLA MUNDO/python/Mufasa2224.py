383a4207c3a86ae8bf44815fe12fd8de260c2b43
Mufasa2224 <juarz35@gmail.com>
2024-11-09 16:09:27 -0500

    Create Mufasa2224.py
    ASDASDASDSADASD

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Mufasa2224.py
@@ -0,0 +1,14 @@
+#https://www.python.org
+
+#comentario en una línea
+
+"""
+Esto también es
+un comentario 
+en varias líneas
+"""
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "
